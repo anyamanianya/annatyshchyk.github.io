6b113c56731cf078728aa624efe07ca6e81f3c65
Anna <58286335+anyamanianya@users.noreply.github.com>
2024-05-01 17:05:49 -0500

    Create hamburger.js

---
+++ scripts/hamburger.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("navContainer");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
